91f9b102939f35ab01000cb1dcbfebfba438aa8c
raushan <kraushan022@gmail.com>
2025-02-28 22:40:04 +0530

    add modal and react portal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-// import reactLogo from './assets/react.svg'
+import React, { useState } from "react";
 import viteLogo from "/vite.svg";
 import { NavLink } from "react-router-dom";
+import Modal from "./Modal";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="flex justify-between px-4 py-4 shadow-md md:px-8 ">
       <img src={viteLogo} alt="viteLogo" />
@@ -37,6 +39,41 @@ const Header = () => {
           >
             Contact
           </NavLink>
+        </li>
+        <li>
+          <button onClick={() => setIsOpen(true)}>Sign In</button>
+          <Modal
+            header={<div className="text-xl font-bold">Sign In</div>}
+            footer={
+              <div className="flex justify-end gap-4">
+                <button
+                  onClick={() => setIsOpen(false)}
+                  className="px-6 py-2 font-semibold bg-gray-300 rounded-md hover:bg-gray-400/80 active:bg-gray-400/60"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={() => setIsOpen(false)}
+                  className="px-6 py-2 font-semibold bg-blue-300 rounded-md hover:bg-blue-400/80 active:bg-blue-400/60"
+                >
+                  Sign In
+                </button>
+              </div>
+            }
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+          >
+            <input
+              placeholder="Username"
+              className="px-2 py-1 border border-gray-600 rounded grow"
+              type="text"
+            />
+            <input
+              placeholder="Password"
+              className="px-2 py-1 border border-gray-600 rounded grow"
+              type="password"
+            />
+          </Modal>
         </li>
       </ul>
     </header>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,31 +1,25 @@
 import React from "react";
+import { createPortal } from "react-dom";
 
-export default function Modal() {
-  return (
-    <div className="fixed inset-0 flex items-center justify-center px-4 bg-black/40">
-      <div className="max-w-2xl p-4 bg-white rounded-lg shadow-lg grow">
-        <div className="text-xl font-bold">Sign In</div>
+export default function Modal({ isOpen, setIsOpen, header, footer, children }) {
+  return createPortal(
+    <div
+      onClick={() => setIsOpen(false)}
+      className={`fixed inset-0 flex items-center justify-center bg-black/40 px-4 ${
+        isOpen ? "" : "hidden"
+      }`}
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="max-w-2xl p-4 bg-white rounded-lg shadow-lg grow"
+      >
+        {header}
         <div className="flex flex-wrap gap-4 px-4 py-4 my-3 -mx-4 border-y">
-          <input
-            placeholder="Username"
-            className="px-2 py-1 border border-gray-600 rounded grow"
-            type="text"
-          />
-          <input
-            placeholder="Password"
-            className="px-2 py-1 border border-gray-600 rounded grow"
-            type="password"
-          />
+          {children}
         </div>
-        <div className="flex justify-end gap-4">
-          <button className="px-6 py-2 font-semibold bg-gray-300 rounded-md hover:bg-gray-400/80 active:bg-gray-400/60">
-            Cancel
-          </button>
-          <button className="px-6 py-2 font-semibold bg-blue-300 rounded-md hover:bg-blue-400/80 active:bg-blue-400/60">
-            Sign In
-          </button>
-        </div>
+        {footer}
       </div>
-    </div>
+    </div>,
+    document.getElementById("portal")
   );
 }
